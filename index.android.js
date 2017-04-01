7843d0f89f4951e34e8638dc3da95ce35fbcca74
Arthur BONNECARRERE <abonneca@student.42.fr>
2017-04-01 18:34:53 +0200

    "Android version"

--- index.android.js
+++ index.android.js
@@ -1,53 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  Navigator
 } from 'react-native';
 
+import MainPage from './app/components/MainPage';
+import CreateTask from './app/components/CreateTask';
+import ResetApp from './app/components/ResetApp';
+import styles from './app/styles/styles';
+
 export default class TodosApp extends Component {
+  renderScene(route, navigator) {
+    if (route.name === 'MainPage') {
+      return (
+        <MainPage navigator={ navigator }  />
+      );
+    }
+    else if (route.name === 'CreateTask') {
+      return (
+        <CreateTask navigator={ navigator } createTask={ route.createTask } />
+      );
+    }
+    else if (route.name === 'ResetApp') {
+    return (
+      <ResetApp navigator={ navigator } deleteAllTasks={ route.deleteAllTasks } />
+    );
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Navigator
+        initialRoute={{ name: 'MainPage' }}
+        renderScene={ this.renderScene }
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('TodosApp', () => TodosApp);
