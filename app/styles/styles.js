35c30770f2f2c8d36f630f922e569b7bc925d8b2
Arthur BONNECARRERE <abonneca@student.42.fr>
2017-04-01 17:16:37 +0200

    add styles sheet

---
+++ app/styles/styles.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  newTaskButton: {
+    backgroundColor: '#2980b9',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  newText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  settings: {
+    alignSelf: 'flex-end',
+    paddingTop: 20,
+  },
+});
